479580f02b6d5c8c2798da640da8734b158cdb36
Dima Barboskin <dmro5@mail.ru>
2018-04-25 15:20:00 +0400

    The whole project

---
+++ src/main/java/main/Calculate.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * @author Dmitry Rogoza
+ * User's actions
+ */
+@FunctionalInterface
+public interface Calculate {
+    double calculate(double a,double b);
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,68 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * There is my first task, which names "Calculator"
+ *
+ * @author Dmitry Rogoza
+ */
+public class Main {
+    public static void main(String[] args) {
+        calculateUsersActions();
+    }
+
+    /**
+     * Calculate numbers
+     *
+     * @author Dimitry Rogoza
+     * @param enteringNumber1, which an user entered first
+     * @param enteringNumber2, which an user entered second
+     * @param action, which an user entered
+     * @return result of action
+     */
+    public static double count(double enteringNumber1, double enteringNumber2, Calculate action){
+        //Method for calculation
+        return action.calculate(enteringNumber1,enteringNumber2);
+    }
+
+    /**
+     * Receive and calculate user's action
+     *
+     * @author Dimitry Rogoza
+     */
+    public static void calculateUsersActions(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write first number in format:\"0,0\"");
+        double number1 = scanner.nextDouble();
+        System.out.println("What do you want to do?");
+        System.out.println("Write \"+\" for sum;");
+        System.out.println("\"-\" for subtract;");
+        System.out.println("\"*\" for multiply;");
+        System.out.println("\"/\" for divide.");
+        String action = scanner.next();
+        System.out.println("Write second number in format:\"0,0\"");
+        double number2 = scanner.nextDouble();
+        switch (action) {
+            case "+":
+                System.out.printf("%.4f",count(number1, number2, (a, b) -> a + b));
+                break;
+            case "-":
+                System.out.printf("%.4f",count(number1, number2, (a, b) -> a - b));
+                break;
+            case "*":
+                System.out.printf("%.4f",count(number1, number2, (a, b) -> a * b));
+                break;
+            case "/":
+                if(number2==0)
+                    throw new IllegalArgumentException("You can't divide by zero");
+                System.out.printf("%.4f",count(number1, number2, (a, b) -> a / b));
+                break;
+                default:
+                    System.out.println("Wrong action. Do it again");
+                    calculateUsersActions();
+                    break;
+        }
+        scanner.close();
+    }
+}
